5863bb93616ee761163446854fbf492caa2436b8
bawanegi <souravdalana@gmail.com>
2022-11-06 03:22:06 +0530

    done3

--- src/navbar2.js
+++ src/navbar2.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 class navbar2 extends React.Component {
 render(){
     return(
@@ -10,16 +9,16 @@ render(){
         </div>
         <div className="tc">
         <ul className="dib " >
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">NATIONAL</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">CRIME</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">ENTERTAINMENT</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">POLITICS</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">BUSINESS</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">TECH</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">FIFA WORLD CUP 2022</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">SPORTS</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">WORLD</a>
-        <a className="ph2 no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">FEATURES</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">NATIONAL</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">CRIME</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">ENTERTAINMENT</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">POLITICS</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">BUSINESS</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">TECH</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">FIFA WORLD CUP 2022</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">SPORTS</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">WORLD</a>
+        <a className="ph1 b no-underline dim near-white" href="FIFA WORLD CUP 2022 SPORTS WORLD FEATURES">FEATURES</a>
         </ul>
         </div>
         <div className="searchbox ">
